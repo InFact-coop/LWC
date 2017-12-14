5a7f31add77b259a123e1be5f1cd6664317b23f5
jen-harris <jennifer.louise.harris@gmail.com>
2017-12-14 16:38:47 +0000

    adds initial styling to welcome page need to add navbar relates #12

--- src/elm/Routes/LandingPage.elm
+++ src/elm/Routes/LandingPage.elm
@@ -8,6 +8,12 @@ import Types exposing (..)
 
 landingPage : Model -> Html Msg
 landingPage model =
-    div [ class "" ]
-        [ h1 [ class "tc f1" ] [ text "Landing" ]
+    div [ class "hands" ]
+        [ div [ class "purple-background w-100 h-100 " ]
+            [ div [ class "white b f2 tc pa3 pt5 tracked" ]
+                [ text "We are here to help." ]
+            , div [ class "white-line pt3 pb4" ] []
+            , div [ class "mt5" ] [ a [ class "purple bg-white-80 tracked pa4  br2 db tc w-80 m0-auto mt4" ] [ text "NEED ADVICE?" ] ]
+            , div [ class "mt4" ] [ a [ class "purple bg-white-80 tracked pa4  br2 db tc w-80 m0-auto mt4" ] [ text "KNOW WHAT YOU NEED?" ] ]
+            ]
         ]
