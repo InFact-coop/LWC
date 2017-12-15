d48a2198f738d0de131fe07b40dc524f4637d9f2
Max Gerber <mgerber94@gmail.com>
2017-12-15 16:01:27 +0000

    Styling touch-ups to #28

--- src/elm/Components/TestimonialSection.elm
+++ src/elm/Components/TestimonialSection.elm
@@ -9,7 +9,7 @@ import Components.Rule exposing (..)
 
 testimonialSection : Testimonial -> Html Msg
 testimonialSection { name, age, imgsrc, therapy, description } =
-    section [ class "testimonial ma3 ba b--solid b--silver br3 pa4" ] [ header [ class "mb3 dib" ] [ img [ class "dib h4 w4 v-btm", src imgsrc, alt name ] [], div [ class "dib ml3" ] [ h1 [ class "dib purple f3 ma0" ] [ text <| name ++ ", " ++ age ], h2 [ class "f4 gray mt2 mb3" ] [ text therapy ], rule "purple" "40%" ] ], description ]
+    section [ class "testimonial ma3 ba b--solid b--silver br3 pa4" ] [ header [ class "mb3 dib" ] [ img [ class "dib h4 w4 v-btm", src imgsrc, alt name ] [], div [ class "dib ml3" ] [ h1 [ class "dib purple f3 ma0" ] [ text <| name ++ ", " ++ age ], h2 [ class "f4 gray mt1 mb3" ] [ text therapy ], rule "purple" "70%" ] ], description ]
 
 
 retrieveTestimonial : Model -> Html Msg
