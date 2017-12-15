9a0ab2da5b2359f3f8181308662eb3212d4830ec
jen-harris <jennifer.louise.harris@gmail.com>
2017-12-15 15:24:01 +0000

    more quotes and women added relates #29

--- src/elm/Data/Quotes.elm
+++ src/elm/Data/Quotes.elm
@@ -4,12 +4,21 @@ module Data.Quotes exposing (..)
 quotesList : List ( String, String )
 quotesList =
     [ ( "./assets/woman1.png"
-      , "         Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat"
+      , "I had got into debt problems with an energy supplier. LWC helped to tackle the issue and managed to get £3,500 of debt written off"
       )
     , ( "./assets/woman2.png"
-      , "       Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. "
+      , "        I didn't know which way to turn after a recent seperation. With LWC's help i now feels much more supported and know that help is always on hand for form filling , advice and money matters"
       )
     , ( "./assets/woman3.png"
-      , "       Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. "
+      , "I had worked in the same job for a number of years, but was struggling with bullying in the workplace. This impacted my Mental Health and ended up unemployed. through LWC I used services that helped me regain confidence and find a part time job."
+      )
+    , ( "./assets/woman4.png"
+      , " I attended living well therapy sessions and accessed Behavioural Activation which helped my negative thinking and emotions."
+      )
+    , ( "./assets/woman5.png"
+      , "I suffer from sciatica pain and a rotator cuff injury, which made me feel miserable. Attending living well sessions helped make me feel more motivated."
+      )
+    , ( "./assets/woman6.png"
+      , "I came into contact with LWC following release from Prison. An LWC caseworker helped me find new accommodation, made contact with the local alcohol support team and helped me claim benefits."
       )
     ]
