79bb44850db31cf0269706fbcc29cd932e83f175
Max Gerber <mgerber94@gmail.com>
2017-12-18 16:57:26 +0000

    2/6 new user stories inputted #28

--- src/elm/Data/Testimonials.elm
+++ src/elm/Data/Testimonials.elm
@@ -13,7 +13,7 @@ emphasis content =
 defaultTestimonial : Testimonial
 defaultTestimonial =
     { id = 4
-    , name = "Linda"
+    , name = ""
     , age = "37"
     , imgsrc = "./assets/woman2.png"
     , therapy = "Living Well"
@@ -39,26 +39,47 @@ defaultTestimonial =
 
 testimonialsList : List Testimonial
 testimonialsList =
-    [ { id = 4
-      , name = "Linda"
-      , age = "37"
-      , imgsrc = "./assets/woman2.png"
-      , therapy = "Living Well"
+    [ { id = 1
+      , name = "Maureen"
+      , age = "34"
+      , imgsrc = "./assets/woman1.png"
+      , therapy = "Money, Debt & Benefit Advice"
       , description =
             section [ class "description ph3 pt0" ]
                 [ p []
-                    [ text "I came to LWC about three years ago after I was diagnosed with "
-                    , emphasis "low mood"
-                    , text " and "
-                    , emphasis "anxiety"
-                    , text ". I was feeling really isolated in social situations and became a bit of a recluse."
+                    [ text "Maureen had gotten into "
+                    , emphasis "debt problems"
+                    , text " with an energy supplier. She saw an advisor from "
+                    , emphasis "Money, Debt & Benefit Advice"
+                    , text " over 4 sessions to tackle the issue."
                     ]
                 , p []
-                    [ text "I began by attending "
-                    , emphasis "Living Well therapy sessions"
-                    , text ", which really really helped me. Later on I went to "
-                    , emphasis "Behavioural Activation"
-                    , text " to address some of the negative thoughts I’d been having. "
+                    [ text "As a result, over £3,500 of her debt was written off and she was successful in her application for a "
+                    , emphasis "£140 Warm Home Discount"
+                    , text " also. The financial pressure she was feeling "
+                    , emphasis "has now gone"
+                    , text " and she feels "
+                    , emphasis "much happier"
+                    , text "."
+                    ]
+                ]
+      }
+    , { id = 2
+      , name = "Emma"
+      , age = "43"
+      , imgsrc = "./assets/woman2.png"
+      , therapy = "Money, Debt & Benefit Advice"
+      , description =
+            section [ class "description ph3 pt0" ]
+                [ p []
+                    [ text "Emma did not know which way to turn after a recent seperation. After just 2 adviser appointments with our "
+                    , emphasis "Money, Debt & Benefit Advice"
+                    , text " Service, benefits worth £4882.80 have been confirmed as being in payment and an application for further benefits has been put in with the advisor's help."
+                    ]
+                , p []
+                    [ text "Emma now feels much more "
+                    , emphasis "supported"
+                    , text " and knows that help is always on hand for form filling , advice and money matters"
                     ]
                 ]
       }
@@ -85,34 +106,11 @@ testimonialsList =
                     ]
                 ]
       }
-    , { id = 2
-      , name = "Lola"
+    , { id = 4
+      , name = "Linda"
       , age = "37"
-      , imgsrc = "./assets/woman1.png"
-      , therapy = "Living a life"
-      , description =
-            section [ class "description ph3 pt0" ]
-                [ p []
-                    [ text "I came to LWC about three years ago after I was diagnosed with "
-                    , emphasis "low mood"
-                    , text " and "
-                    , emphasis "anxiety"
-                    , text ". I was feeling really isolated in social situations and became a bit of a recluse."
-                    ]
-                , p []
-                    [ text "I began by attending "
-                    , emphasis "Living Well therapy sessions"
-                    , text ", which really really helped me. Later on I went to "
-                    , emphasis "Behavioural Activation"
-                    , text " to address some of the negative thoughts I’d been having. "
-                    ]
-                ]
-      }
-    , { id = 1
-      , name = "Rivka"
-      , age = "27"
-      , imgsrc = "./assets/woman1.png"
-      , therapy = "Living...just about"
+      , imgsrc = "./assets/woman2.png"
+      , therapy = "Living Well"
       , description =
             section [ class "description ph3 pt0" ]
                 [ p []
@@ -136,29 +134,6 @@ testimonialsList =
       , age = "23"
       , imgsrc = "./assets/woman1.png"
       , therapy = "Meh"
-      , description =
-            section [ class "description ph3 pt0" ]
-                [ p []
-                    [ text "I came to LWC about three years ago after I was diagnosed with "
-                    , emphasis "low mood"
-                    , text " and "
-                    , emphasis "anxiety"
-                    , text ". I was feeling really isolated in social situations and became a bit of a recluse."
-                    ]
-                , p []
-                    [ text "I began by attending "
-                    , emphasis "Living Well therapy sessions"
-                    , text ", which really really helped me. Later on I went to "
-                    , emphasis "Behavioural Activation"
-                    , text " to address some of the negative thoughts I’d been having. "
-                    ]
-                ]
-      }
-    , { id = 5
-      , name = "Marticia"
-      , age = "37"
-      , imgsrc = "./assets/woman1.png"
-      , therapy = "Living Dandy"
       , description =
             section [ class "description ph3 pt0" ]
                 [ p []
