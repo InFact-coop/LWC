1749c1a4e450c80069d20d29605f7a66e14a804d
Max Gerber <mgerber94@gmail.com>
2017-12-19 12:28:51 +0000

    Emphasised bottom 4 #38

--- src/elm/Data/Services.elm
+++ src/elm/Data/Services.elm
@@ -36,7 +36,34 @@ servicesList =
     , { id = 4
       , name = "Money, Debt & Benefit Advice"
       , isVisible = False
-      , description = section [ class "description-service pa3 gray" ] [ p [] [ text "Lancashire Women's Centres offer a Financial Conduct Authority (FCA) regulated debt counselling and money advice appointments offering a range of services.\n\nSuch as:\n\nBenefit advice including rent arrears,\nBenefit applications\nMandatory reconsiderations and appeals\nDebt issues and options,\nBack to work calculations\nBudgeting advice.\nlegal Clinics - We work with a number of local solicitors who provide free legal clinics at our centres.\nSessions are delivered out of each of our centres and work in partnership with Shelter within Blackburn Library.\nWe offer 1-to-1 sessions by experienced staff offering support and impartial advice to those in need." ] ]
+      , description =
+            section [ class "description-service pa3 gray" ]
+                [ p []
+                    [ text "Lancashire Women's Centres offer a Financial Conduct Authority (FCA) regulated "
+                    , emphasis "debt counselling and money advice"
+                    , text " appointments offering a range of services. These include:"
+                    ]
+                , ul
+                    []
+                    [ li [] [ text "Benefit advice including rent arrears" ]
+                    , li [] [ text "Benefit applications" ]
+                    , li [] [ text "Mandatory reconsiderations and appeals" ]
+                    , li [] [ text "Debt issues and options" ]
+                    , li [] [ text "Back to work calculations" ]
+                    , li [] [ text "Budgeting advice" ]
+                    , li [] [ text "Legal Clinics" ]
+                    ]
+                , p
+                    []
+                    [ text "We work with a number of local solicitors who provide "
+                    , emphasis "free legal clinics"
+                    , text " at our centres. Sessions are delivered out of each of our centres and work in partnership with Shelter within Blackburn Library. We offer "
+                    , emphasis "1-to-1 sessions"
+                    , text " by experienced staff offering "
+                    , emphasis "support and impartial advice"
+                    , text " to those in need."
+                    ]
+                ]
       }
     , { id = 5
       , name = "Volunteering & Mentoring"
@@ -44,18 +71,64 @@ servicesList =
       , description =
             section [ class "description-service pa3 gray" ]
                 [ p []
-                    [ text "Use your Skills, Life Experiences and Spare Time to Support Others, Volunteers are at the heart of our centres. If you want to get involved, meet new people, learn new skills or rediscover old ones then please get in touch with your local centre. We have lots of projects and work streams were we are dependent on Volunteers and Mentor and we also need general volunteers and mentors as there are times in all our lives when we could really benefit from the help of another person. We might just want someone to discuss options with, someone to talk through ideas for the future or support with forms and appointments or advice on the internal services we can offer. Mentoring offers you the opportunity to develop a relationship that is based on trust, confidentiality and mutual involvement to set goals and decide achievements. Your sessions will involve setting personal goals, guidance and action planning to help a successful outcome."
+                    [ text "Use your "
+                    , emphasis " Skills"
+                    , text ", "
+                    , emphasis "Life Experiences"
+                    , text " and "
+                    , emphasis "Spare Time"
+                    , text " to "
+                    , emphasis "Support Others"
+                    , text ". Volunteers are at the heart of our centres. If you want to get involved, meet new people, learn new skills or rediscover old ones then please get in touch with your local centre. We have "
+                    , emphasis "lots of projects"
+                    , text " and work streams where we are dependent on Volunteers and Mentors, just as we also need general volunteers and mentors as there are times in all our lives when we could really benefit from the "
+                    , emphasis "help of another person"
+                    , text ". We might just want someone to "
+                    , emphasis "discuss options with"
+                    , text ", someone to talk through "
+                    , emphasis "ideas for the future"
+                    , text " or support with forms and appointments or advice on the internal services we can offer. Mentoring offers you the opportunity to develop a "
+                    , emphasis "relationship that is based on trust"
+                    , text ", confidentiality and mutual involvement to set goals and decide achievements. Your sessions will involve "
+                    , emphasis "setting personal goals, guidance"
+                    , text " and "
+                    , emphasis "action planning"
+                    , text " to help a successful outcome."
                     ]
                 ]
       }
     , { id = 6
       , name = "Women in Crisis"
       , isVisible = False
-      , description = section [ class "description-service pa3 gray" ] [ p [] [ text " We continue to build upon the recommendations of Baroness Corston’s 2007 report into women in the criminal justice system and have developed a package of services which provide a gendered considered response to the issues of female offending behaviour. Using this approach, we support a number of localised multi agency programmes where we are able to use our expertise in the provision of gender specific interventions to improve outcomes for women. We now offer a package of services which work with women at all points in the criminal justice system, as well as women at risk of offending, placing staff within police custody and prison settings to support engagement as well as further extending our community provision." ] ]
+      , description =
+            section [ class "description-service pa3 gray" ]
+                [ p []
+                    [ text " We continue to build upon the recommendations of Baroness Corston’s 2007 report into "
+                    , emphasis "women in the criminal justice system"
+                    , text " and have developed a package of services which provide a gendered considered response to the issues of "
+                    , emphasis "female offending behaviour"
+                    , text ". Using this approach, we support a number of localised multi agency programmes where we are able to use our expertise in the provision of gender specific interventions to "
+                    , emphasis "improve outcomes for women"
+                    , text ". We now offer a package of services which work with women at all points in the criminal justice system, as well as "
+                    , emphasis "women at risk of offending"
+                    , text ", placing staff within police custody and prison settings to support engagement as well as further extending our community provision."
+                    ]
+                ]
       }
     , { id = 7
       , name = "Meeting Others"
       , isVisible = False
-      , description = section [ class "description-service pa3 gray" ] [ p [] [ text "Our Peer Support Groups i.e. BETTER TOGETHER - for continued Mental Health & Wellbeing Come along to this informal, friendly peer support group for a brew & chat" ] ]
+      , description =
+            section [ class "description-service pa3 gray" ]
+                [ p []
+                    [ text "Our Peer Support Groups (i.e. "
+                    , emphasis "BETTER TOGETHER"
+                    , text ") are for continued "
+                    , emphasis "Mental Health & Wellbeing"
+                    , text ". Come along to this informal, friendly "
+                    , emphasis "Peer Support group"
+                    , text " for a brew & chat"
+                    ]
+                ]
       }
     ]
