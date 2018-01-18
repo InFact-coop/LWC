ff4847869cf7c0029941828292bd12df31e0691b
jen-harris <jennifer.louise.harris@gmail.com>
2018-01-18 17:00:19 +0000

    adds 2 buttons back to small screen relates #77

--- src/elm/Routes/DetailedQuotePage.elm
+++ src/elm/Routes/DetailedQuotePage.elm
@@ -14,5 +14,7 @@ detailedQuotePage model =
         , div [ class "gray fw1 mt3 mb3 mb4-ns w-50-l" ] [ text "Below is a real story from someone we've helped. We provide all the services that they used. If you think something they've used would help you as well or if you're still not sure please get in touch with us." ]
         , div [ class "purple-line mb4" ] []
         , retrieveTestimonial model
+        , div [ class "dn-ns" ] [ purpleButton "See full list of services" "services" ]
+        , div [ class "dn-ns" ] [ purpleButton "Contact us" "talk-to-us" ]
         , clearWhiteButton "Back to stories" "quotes"
         ]
