4a513027b68c482c1539c97ae945fc0a21e70938
jen-harris <jennifer.louise.harris@gmail.com>
2018-01-18 15:21:38 +0000

    adds responsive design to landing page relates #78

--- src/elm/Routes/LandingPage.elm
+++ src/elm/Routes/LandingPage.elm
@@ -8,13 +8,31 @@ import Types exposing (..)
 
 landingPage : Model -> Html Msg
 landingPage model =
-    div [ class "hands" ]
-        [ div [ class "purple-background w-100 h-100 " ]
-            [ div [ class "white b f2 tc pl3 pr3 pt5 tracked" ]
-                [ text "We are here to help." ]
-            , div [ class "white-line pb4" ] []
-            , whiteButton "SEE REAL STORIES" "quotes"
-            , whiteButton "SEE OUR SERVICES" "services"
-            , whiteButton "CONTACT US" "talk-to-us"
+    div []
+        [ div [ class "hands dn-l" ]
+            [ div [ class "purple-background w-100 h-100 " ]
+                [ div [ class "white b f2 tc pl3 pr3 pt5 tracked" ]
+                    [ text "We are here to help." ]
+                , div [ class "white-line pb4" ] []
+                , whiteButton "SEE REAL STORIES" "quotes"
+                , whiteButton "SEE OUR SERVICES" "services"
+                , whiteButton "CONTACT US" "talk-to-us"
+                ]
+            ]
+        , div [ class " group dib-l dn" ]
+            [ div [ class "purple-background w-100 h-100 " ]
+                [ div [ class "white b f1 tc pl3 pr3 pt5 tracked w-50 m0-auto" ]
+                    [ text "We are here to help." ]
+                , div [ class "white-line pb4 " ] []
+                , div [ class "tc" ]
+                    [ div [ class "pt5 w-50 tc dib " ] [ whiteButton "SEE REAL STORIES" "quotes" ]
+                    ]
+                , div [ class "tc" ]
+                    [ div [ class "pt1 w-50 tc dib " ] [ whiteButton "SEE OUR SERVICES" "services" ]
+                    ]
+                , div [ class "tc" ]
+                    [ div [ class "pt1 w-50 tc dib " ] [ whiteButton "CONTACT US" "talk-to-us" ]
+                    ]
+                ]
             ]
         ]
