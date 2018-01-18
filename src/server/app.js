9d37bc1aefcdaa18aa020a295987d45fdc098681
Matt Davis <matthewpeterdavis@gmail.com>
2018-01-18 21:36:23 +0000

    Add comments and better error handling #75

--- src/server/app.js
+++ src/server/app.js
@@ -3,6 +3,7 @@ const path = require("path");
 const express = require("express");
 const bodyParser = require("body-parser");
 const api_router = require("./routers/api_router");
+const util = require("util");
 
 const app = express();
 
@@ -10,11 +11,28 @@ app.use(express.static(path.join(__dirname, "../../public")));
 app.use(bodyParser.json());
 app.use(express.static("public"));
 
+// API Router
 app.use("/api/v1/", api_router);
 
+// Validation Error Handler
 app.use((err, req, res, next) => {
-  console.log(err.errors[0].messages[0]);
-  res.status(400).json(err);
+  if ((err.message = "validation error")) {
+    return res.status(400).json(err);
+  }
+  return next();
 });
 
+// Generic Error Handler
+app.use((err, req, res, next) => {
+  console.log(
+    "Server Error: ",
+    util.inspect(err, { showHidden: false, depth: null })
+  );
+  return res.status(500).send("500 Internal Server Error");
+});
+
+// 404 route
+app.use((req, res, next) => {
+  res.status(404).send("404 Page Not Found");
+});
 module.exports = app;
