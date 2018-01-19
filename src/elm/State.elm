950c1f75af9410de9a10df872776a99fff4181c4
jen-harris <jennifer.louise.harris@gmail.com>
2018-01-19 14:38:43 +0000

    changes imgage on thank you page relates #85

--- src/elm/State.elm
+++ src/elm/State.elm
@@ -14,7 +14,7 @@ import Types exposing (..)
 
 initModel : Model
 initModel =
-    { route = LandingRoute
+    { route = ThankYouRoute
     , formSent = NotSent
     , services = servicesList
     , testimonials = testimonialsList
