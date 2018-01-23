e202fcfe025da40c6307497fa1d1e09636e21098
Matt Davis <matthewpeterdavis@gmail.com>
2018-01-23 16:05:17 +0000

    refactor form
    relates #74

--- src/elm/Routes/FormPage.elm
+++ src/elm/Routes/FormPage.elm
@@ -10,7 +10,7 @@ import Types exposing (..)
 formPage : Model -> Html Msg
 formPage model =
     div [ class "ma4" ]
-        [ div [ class "b gray f3 mb3" ]
+        [ div [ class "b gray f3 mb3", id "whole-page" ]
             [ text "Your details" ]
         , div
             [ class "gray fw1 mb4" ]
@@ -18,93 +18,98 @@ formPage model =
         , div
             [ class "purple-line fl pl3 mb4" ]
             []
-        , Html.form [ onSubmit SendHelpForm, class " w-50-l m0-auto" ]
-            [ div [ class "ba br1 flex w-100 flex-wrap pa1 b--silver" ]
-                [ div [ class "purple fw1" ] [ text "Name" ]
-                , input [ value model.newHelpForm.name, type_ "text", class "w-100 f4 gray fw1 bn", onInput (ChangeFormName model.newHelpForm) ] []
-                ]
-            , div [ class "ba br1 flex w-100 flex-wrap pa1 b--silver mt3" ]
-                [ div [ class "purple fw1" ] [ text "Date of Birth" ]
-                , input [ value model.newHelpForm.dob, type_ "date", class "w-100 f4 gray fw1 bn", onInput (ChangeFormDOB model.newHelpForm) ] []
-                ]
-            , div [ class "ba br1 flex w-100 flex-wrap pa1 b--silver mt3" ]
-                [ div [ class "purple fw1" ] [ text "Contact Number" ]
-                , input [ value model.newHelpForm.contactNumber, type_ "text", class "w-100 f4 gray fw1 bn", onInput (ChangeFormNumber model.newHelpForm) ] []
-                ]
-type alias FormField =
-    String
-
-
-formErrors : FormField -> List ValError -> ( Html msg, Bool )
-formErrors field errors =
-    ( errors
-        |> List.filter (\error -> error.field == field)
-        |> List.map (\error -> div [] (List.map (\errMess -> div [ class "purple f5" ] [ text errMess ]) error.messages))
-        |> div []
-    , errors
-        |> List.filter (\error -> error.field == field)
-        |> List.isEmpty
-        |> not
-    )
-
-
-            -- New input fields
-            , div [ class "pt4 pb3" ] [ text "The following questions are optional, but they are really helpful for us to put you in touch with the right person." ]
-
-            -- choice of services
-            , div [ class "ba br1 flex w-100 flex-wrap pa1 b--silver  pb3 pt1" ]
-                [ h4 [ class "purple fw1 mt1 w-100" ] [ text "Which services are you interested in?" ]
-                , section [ class "pl3 w-100" ]
-                    [ -- emotional wellbeing
-                      buttonItem model.newHelpForm.emotionalWellbeing
-                        (CheckboxEmotion model.newHelpForm)
-                        "Emotional Wellbeing"
-                    , buttonItem model.newHelpForm.personal
-                        (CheckboxPersonal model.newHelpForm)
-                        "Personal Development"
-                    , buttonItem model.newHelpForm.employment
-                        (CheckboxEmployment model.newHelpForm)
-                        "Employment Support"
-                    , buttonItem model.newHelpForm.money
-                        (CheckboxMoney model.newHelpForm)
-                        "Money, Debt and Benefit Advice"
-                    , buttonItem model.newHelpForm.volunteering
-                        (CheckboxVolunteering model.newHelpForm)
-                        "Volunteering and Mentoring"
-                    , buttonItem model.newHelpForm.meeting
-                        (CheckboxMeeting model.newHelpForm)
-                        "Meeting Others"
-                    ]
-                ]
-
-            -- Additional information
-            , div [ class "ba br1 flex w-100 flex-wrap pa1 b--silver mt3" ]
-                [ div [ class "purple fw1 pb2" ] [ text "Is there anything else you would like to tell us?" ]
-                , textarea
-                    [ class "sans-serif w-100 mh3 h4 f5 gray fw1 bn"
-                    , name "additionalInfo"
-                    , attribute "rows" "5"
-                    , placeholder "Please let us know here"
-                    , value model.newHelpForm.moreInfo
-                    , onInput (ChangeFormMore model.newHelpForm)
-                    ]
-                    []
-                ]
-            , buttonItem model.newHelpForm.gdpr (CheckboxGDPR model.newHelpForm) "Please check here to consent to LOREM IPSUM"
-
-            -- same as before
-            , div [ class "tc" ]
-                [ button
-                    [ type_ "submit"
-                    , class "pointer f5 ba br1 w-80 w-40-l br2 pa3 tc bg-purple white mt3 br1 dim"
-                    , classList [ ( "bg-light-purple", not model.newHelpForm.gdpr ) ]
-                    , disabled <| not model.newHelpForm.gdpr
-                    ]
-                    [ text "Submit" ]
-                ]
-            , sendingMsg model.formSent
-            ]
+        , detailsForm model
         , clearWhiteButton "Back" "talk-to-us"
+        ]
+
+
+detailsForm : Model -> Html Msg
+detailsForm model =
+    Html.form [ onSubmit SendHelpForm, class " w-50-l m0-auto" ]
+        [ formInput model "Name" "text" model.newHelpForm.name ChangeFormName wrapperClass_text_top errClass_text titleClass_text inputClass_text
+        , formInput model "Date Of Birth" "date" model.newHelpForm.dob ChangeFormDOB wrapperClass_text errClass_text titleClass_text inputClass_text
+        , formInput model "Contact Number" "text" model.newHelpForm.contactNumber ChangeFormNumber wrapperClass_text errClass_text titleClass_text inputClass_text
+        , formInput model "Email" "text" model.newHelpForm.email ChangeFormEmail wrapperClass_text errClass_text titleClass_text inputClass_text
+        , formInput model "Postcode" "text" model.newHelpForm.postcode ChangeFormPostcode wrapperClass_text errClass_text titleClass_text inputClass_text
+
+        -- New input fields
+        , div [ class "pt4 pb3" ] [ text "The following questions are optional, but they are really helpful for us to put you in touch with the right person." ]
+
+        -- choice of services
+        , div [ class "ba br1 flex w-100 flex-wrap pa1 b--silver  pb3 pt1" ]
+            [ h4 [ class "purple fw1 mt1 w-100" ] [ text "Which services are you interested in?" ]
+            , section [ class "pl3 w-100" ]
+                [ -- emotional wellbeing
+                  buttonItem model.newHelpForm.emotionalWellbeing
+                    (CheckboxEmotion model.newHelpForm)
+                    "Emotional Wellbeing"
+                , buttonItem model.newHelpForm.personal
+                    (CheckboxPersonal model.newHelpForm)
+                    "Personal Development"
+                , buttonItem model.newHelpForm.employment
+                    (CheckboxEmployment model.newHelpForm)
+                    "Employment Support"
+                , buttonItem model.newHelpForm.money
+                    (CheckboxMoney model.newHelpForm)
+                    "Money, Debt and Benefit Advice"
+                , buttonItem model.newHelpForm.volunteering
+                    (CheckboxVolunteering model.newHelpForm)
+                    "Volunteering and Mentoring"
+                , buttonItem model.newHelpForm.meeting
+                    (CheckboxMeeting model.newHelpForm)
+                    "Meeting Others"
+                ]
+            ]
+
+        -- Additional information
+        , div [ class "ba br1 flex w-100 flex-wrap pa1 b--silver mt3" ]
+            [ div [ class "purple fw1 pb2" ] [ text "Is there anything else you would like to tell us?" ]
+            , textarea
+                [ class "sans-serif w-100 mh3 h4 f5 gray fw1 bn"
+                , name "additionalInfo"
+                , attribute "rows" "5"
+                , placeholder "Please let us know here"
+                , value model.newHelpForm.moreInfo
+                , onInput (ChangeFormMore model.newHelpForm)
+                ]
+                []
+            ]
+        , buttonItem model.newHelpForm.gdpr (CheckboxGDPR model.newHelpForm) "Please check here to consent to LOREM IPSUM"
+
+        -- same as before
+        , div [ class "tc" ]
+            [ button
+                [ type_ "submit"
+                , class "pointer f5 ba br1 w-80 w-40-l br2 pa3 tc bg-purple white mt3 br1 dim"
+                , classList [ ( "bg-light-purple", not model.newHelpForm.gdpr ) ]
+                , disabled <| not model.newHelpForm.gdpr
+                ]
+                [ text "Submit" ]
+            ]
+        , sendingMsg model.formSent
+        ]
+
+
+formInput model fieldName fieldType fieldValue msg wrapperClass errClass titleClass inputClass =
+    let
+        ( errHtml, errExists ) =
+            formErrors fieldName model.validationErrors
+    in
+    div []
+        [ div [ class wrapperClass, classList [ ( errClass, errExists ) ] ]
+            [ div [ class titleClass ] [ text fieldName ]
+            , label [ for fieldName ]
+                []
+            , input
+                [ value fieldValue
+                , type_ fieldType
+                , class inputClass
+                , onInput (msg model.newHelpForm)
+                , name fieldName
+                ]
+                []
+            ]
+        , errHtml
         ]
 
 
@@ -132,3 +137,40 @@ buttonItem state msg textValue =
             , p [ class "ma0 pa0 purple f5  lh-copy ph2 v-mid dib" ] [ text textValue ]
             ]
         ]
+
+
+type alias FormField =
+    String
+
+
+formErrors : FormField -> List ValError -> ( Html msg, Bool )
+formErrors field errors =
+    ( errors
+        |> List.filter (\error -> error.field == field)
+        |> List.map (\error -> div [] (List.map (\errMess -> div [ class "purple f5" ] [ text errMess ]) error.messages))
+        |> div []
+    , errors
+        |> List.filter (\error -> error.field == field)
+        |> List.isEmpty
+        |> not
+    )
+
+
+wrapperClass_text_top =
+    "ba br1 flex w-100 flex-wrap pa1 b--silver"
+
+
+wrapperClass_text =
+    "ba br1 flex w-100 flex-wrap pa1 b--silver mt3"
+
+
+errClass_text =
+    "b--purple bw2"
+
+
+titleClass_text =
+    "purple fw1"
+
+
+inputClass_text =
+    "w-100 f4 gray fw1 bn"
