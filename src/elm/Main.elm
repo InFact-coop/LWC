2619bd85f364fd722b71f97892e2f70b540a6aa5
Matt Davis <matthewpeterdavis@gmail.com>
2018-01-24 17:43:34 +0000

    fetch players on init
    relates #101

--- src/elm/Main.elm
+++ src/elm/Main.elm
@@ -1,19 +1,18 @@
 module Main exposing (..)
 
-import Navigation
-
-
 -- My Elm Files
 
+import Commands exposing (..)
+import Navigation
 import State exposing (..)
-import View exposing (..)
 import Types exposing (..)
+import View exposing (..)
 
 
 main : Program Never Model Msg
 main =
     Navigation.program UrlChange
-        { init = always ( initModel, Cmd.none )
+        { init = always ( initModel, fetchTestimonialQuotes )
         , view = view
         , update = update
         , subscriptions = always Sub.none
