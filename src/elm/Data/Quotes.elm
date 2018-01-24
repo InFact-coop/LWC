b6a4b79b13eb5053f26256db58d5699166150e1f
Matt Davis <matthewpeterdavis@gmail.com>
2018-01-24 16:25:02 +0000

    fix typo in quotes
    relates #101

--- src/elm/Data/Quotes.elm
+++ src/elm/Data/Quotes.elm
@@ -14,7 +14,7 @@ quoteMaker int imgsrc quote =
 quotesList : List Quote
 quotesList =
     [ quoteMaker 1 "./assets/woman1.png" "I had got into debt problems with an energy supplier. LWC helped to tackle the issue and managed to get £3,500 of debt written off"
-    , quoteMaker 2 "./assets/woman2.png" "I didn't know which way to turn after a recent seperation. With LWC's help i now feels much more supported and know that help is always on hand for form filling , advice and money matters"
+    , quoteMaker 2 "./assets/woman2.png" "I didn't know which way to turn after a recent separation. With LWC's help i now feels much more supported and know that help is always on hand for form filling , advice and money matters"
     , quoteMaker 3 "./assets/woman3.png" "I had worked in the same job for a number of years, but was struggling with bullying in the workplace. This impacted my Mental Health and ended up unemployed. through LWC I used services that helped me regain confidence and find a part time job."
     , quoteMaker 4 "./assets/woman4.png" "I attended living well therapy sessions and accessed Behavioural Activation which helped my negative thinking and emotions."
     , quoteMaker 5 "./assets/woman5.png" "I suffer from sciatica pain and a rotator cuff injury, which made me feel miserable. Attending Living Well sessions helped make me feel more motivated."
